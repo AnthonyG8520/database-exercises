30f0f77883670c25104356eaad302b75bba9260b
anthonygonzales <anthony.j.gonzales22@gmail.com>
2022-03-18 14:37:59 -0500

    2nd part of where clauses

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,10 +1,22 @@
 USE employees;
 
-SELECT emp_no, first_name, last_name FROM employees
-WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+SELECT emp_no, first_name, last_name, gender FROM employees
+WHERE gender = 'M'
+AND (first_name = 'Irena'
+OR first_name = 'Vidya'
+OR first_name = 'Maya');
 
 SELECT emp_no, first_name, last_name FROM employees
 WHERE last_name LIKE 'E%';
 
 SELECT emp_no, first_name, last_name FROM employees
 WHERE last_name LIKE '%q%';
+
+SELECT emp_no, first_name, last_name FROM employees
+WHERE last_name LIKE 'E%' OR last_name LIKE '%E';
+
+SELECT emp_no, first_name, last_name FROM employees
+WHERE last_name LIKE 'E%' AND last_name LIKE '%E';
+
+SELECT emp_no, first_name, last_name FROM employees
+WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
