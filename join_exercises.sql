6faaf64d7142f6b9bfdee1a7974c316cf9e77785
anthonygonzales <anthony.j.gonzales22@gmail.com>
2022-03-23 16:10:09 -0500

    finished join exercises

--- join_exercises.sql
+++ join_exercises.sql
@@ -17,12 +17,23 @@ WHERE YEAR (dept_manager.to_date) = 9999
 AND employees.gender = 'F';
 
 
-SELECT title, COUNT(title)
-FROM titles t
-JOIN dept_emp de ON t.emp_no = de.emp_no
-JOIN departments d ON de.dept_no = d.dept_no
-WHERE d.dept_name = 'Customer Service'
-AND t.to_date = '9999-01-01'
-AND de.to_date = '9999-01-01'
-GROUP BY t.title;
+SELECT title , COUNT(title)
+FROM titles
+JOIN dept_emp ON titles.emp_no = dept_emp.emp_no
+JOIN departments ON dept_emp.dept_no = departments.dept_no
+WHERE departments.dept_name = 'Customer Service'
+AND YEAR (dept_emp.to_date) = 9999
+AND YEAR (titles.to_date) = 9999
+GROUP BY titles.title;
+
+
+SELECT departments.dept_name AS Department_Name,
+CONCAT (employees.first_name, ' ', employees.last_name) AS Department_Manager,salaries.salary
+FROM employees
+JOIN salaries ON employees.emp_no = salaries.emp_no
+JOIN dept_manager ON employees.emp_no = dept_manager.emp_no
+JOIN departments ON dept_manager.dept_no = departments.dept_no
+WHERE YEAR (dept_manager.to_date) = 9999
+AND YEAR (salaries.to_date) = 9999;
+
 
