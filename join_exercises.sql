350b05b5b097ebbcdc30932412eb26463a0ad0f2
anthonygonzales <anthony.j.gonzales22@gmail.com>
2022-03-23 11:31:12 -0500

    more work on joins exercise

--- join_exercises.sql
+++ join_exercises.sql
@@ -3,7 +3,14 @@ USE employees;
 SELECT CONCAT (employees.first_name, ' ', employees.last_name), departments.dept_name
 FROM employees
 JOIN dept_manager ON employees.emp_no = dept_manager.emp_no
-JOIN departments ON dept_manager.dept_no = departments.dept_no;
+JOIN departments ON dept_manager.dept_no = departments.dept_no
+WHERE YEAR (dept_manager.to_date) = 9999;
 
+SELECT employees.gender, CONCAT (employees.first_name, ' ', employees.last_name), departments.dept_name
+FROM employees
+JOIN dept_manager ON employees.emp_no = dept_manager.emp_no
+JOIN departments ON dept_manager.dept_no = departments.dept_no
+WHERE YEAR (dept_manager.to_date) = 9999
+AND employees.gender = 'F';
 
 
