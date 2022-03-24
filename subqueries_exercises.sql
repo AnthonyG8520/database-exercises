a34d5eea0b6ce56c49a738c21dfe07a97f910e36
anthonygonzales <anthony.j.gonzales22@gmail.com>
2022-03-24 11:37:47 -0500

    finished bonus sub queries

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -13,8 +13,14 @@ FROM employees
 WHERE emp_no IN (SELECT emp_no FROM dept_manager WHERE YEAR (to_date) = 9999)
 AND gender = 'F';
 
+
 #BONUSES
 SELECT dept_name
 FROM departments
 WHERE dept_no IN (SELECT dept_no FROM dept_manager WHERE YEAR (to_date) = 9999
 AND emp_no IN (SELECT emp_no FROM employees WHERE gender = 'F'));
+
+SELECT first_name, last_name
+FROM employees
+WHERE emp_no IN (SELECT emp_no FROM salaries WHERE salary IN (SELECT MAX(salary) FROM salaries));
+
